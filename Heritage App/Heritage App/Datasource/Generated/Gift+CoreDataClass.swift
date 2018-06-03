d4c96f992906e7606db28427c5dce2dc25221164
Yaroslav Brekhunchenko <brekhunchenko@Yaroslavs-Mac-Pro.local>
2018-06-03 13:15:48 +0300

    Networking added (in progress).

---
+++ Heritage App/Heritage App/Datasource/Generated/Gift+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Gift+CoreDataClass.swift
+//  PMI
+//
+//  Created by Yaroslav Brekhunchenko on 4/16/18.
+//  Copyright © 2018 Yaroslav Brekhunchenko. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Gift)
+public class Gift: NSManagedObject {
+
+}
