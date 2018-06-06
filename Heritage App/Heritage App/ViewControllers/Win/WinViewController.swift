0b1fc38c7e29fa69dc1a2779f4d9c9f39a67ad94
Yaroslav Brekhunchenko <brekhunchenko@Yaroslavs-Mac-Pro.local>
2018-06-06 11:49:08 +0300

    Animation added.

--- Heritage App/Heritage App/ViewControllers/Win/WinViewController.swift
+++ Heritage App/Heritage App/ViewControllers/Win/WinViewController.swift
@@ -10,15 +10,46 @@ import UIKit
 
 class WinViewController: ReportViewController {
 
+    @IBOutlet weak var heritageImageView: UIImageView!
+    @IBOutlet weak var chesterfieldImageView: UIImageView!
+    @IBOutlet weak var packImageView: UIImageView!
+    @IBOutlet weak var lastImageView: UIImageView!
+    @IBOutlet weak var disconnectButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.disconnectButton.alpha = 0.0
+        self.lastImageView.alpha = 0.0
+        self.packImageView.alpha = 0.0
+        self.chesterfieldImageView.alpha = 0.0
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.report.time = NSDate()
-        DataStoreManager.sharedInstance.saveContext()
+        UIView.animate(withDuration: 0.75, delay: 0.7, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            self.heritageImageView.alpha = 0
+        }) { (flag) in
+            UIView.animate(withDuration: 0.75, delay: 1.0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                self.packImageView.alpha = 1.0
+                self.chesterfieldImageView.alpha = 1.0
+            }, completion: { (flag) in
+                UIView.animate(withDuration: 0.75, delay: 1.0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                    self.chesterfieldImageView.alpha = 0.0
+                }, completion: { (flag) in
+                    UIView.animate(withDuration: 0.75, delay: 1.0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                        self.lastImageView.alpha = 1.0
+                        self.disconnectButton.alpha = 1.0
+                    }, completion: { (flag) in
+                        //
+                    })
+                })
+            })
+        }
+        
+//        self.report.time = NSDate()
+//        DataStoreManager.sharedInstance.saveContext()
     }
     
     //MARK: Actions
